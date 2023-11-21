2c839435905b4395a0901c92ea40ed6b1deb06a8
Daniel Alejandro <daniel@debian>
2023-11-21 13:12:06 -0500

    crd_query_api

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from .views import TaskViewSet
+
+
+router = routers.DefaultRouter()
+router.register(f'tasks',TaskViewSet,'tasks')
+
+urlpatterns = router.urls
